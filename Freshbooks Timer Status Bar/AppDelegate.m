4d4f8cc30e79080dec0c2150423bc247223ea46d
Carlos Solares <csolares23@gmail.com>
2014-07-17 17:01:44 -0700

    added logos and removed old commented out code that added an icon to the status bar

--- Freshbooks Timer Status Bar/AppDelegate.m
+++ Freshbooks Timer Status Bar/AppDelegate.m
@@ -19,12 +19,6 @@
     _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     _statusItem.title = @"00:00:00";
     
-//    NSImage *menuIcon = [NSImage imageNamed:@"Menu Icon"];
-//    NSImage *highlightIcon = [NSImage imageNamed:@"Menu Icon"]; // Yes, we're using the exact same image asset.
-//    [highlightIcon setTemplate:YES]; // Allows the correct highlighting of the icon when the menu is clicked.
-    
-//    [[self statusItem] setImage:menuIcon];
-//    [[self statusItem] setAlternateImage:highlightIcon];
     [[self statusItem] setMenu:[self menu]];
     [[self statusItem] setHighlightMode:YES];
     
